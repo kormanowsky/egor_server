9b5e505f41671e239747f91586d10dc0ce5b1b27
Mikhail Kormanowsky <mkmkor-kr@yandex.ru>
2018-08-17 17:52:47 +0300

    Update index.php

--- http/index.php
+++ http/index.php
@@ -9,4 +9,17 @@
         <th>Location</th>
       </tr>
       <?php
-      $places = D
+      $places = Database::select(["table"=>"places"]);
+      if($places):
+        foreach($places as $place):
+        ?>
+        <tr>
+          <td><a target="_blank" href="<?php echo $place->link; ?>"><?php echo $place->name;?></a></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <?php enforeach; ?>
+      </table>
+      <?php else: ?>
+        <h3>There are no places</h3>
+      <?php endif; ?>
