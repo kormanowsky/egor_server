238015215572b692cf5b0f926b387446249293ef
Mikhail Kormanowsky <mkmkor-kr@yandex.ru>
2018-08-17 17:04:51 +0300

    Create form.php

---
+++ http/form.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+  <body>
+    <h1>Submit your place here</h1>
+    <form method="POST" action="handler.php">
+      <label>Place name:<input type="text" name="name"></label>
+      <label>Place link:<input type="text" name="link"></label>
+      <label>Place location:<input type="text" name="location"></label>
+      
