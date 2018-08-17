df04d61c8f89ac2daaf441ec3c8d94f3d2ed0439
Mikhail Kormanowsky <mkmkor-kr@yandex.ru>
2018-08-17 17:12:48 +0300

    Create index.php

---
+++ http/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <body>
+    <h1>List of places</h1>
+    <table>
+      <tr>
+        <th>Name</th>
+        <th>Item</th>
+        <th>Location</th>
+      </tr>
+      <?php
+      $places = D
