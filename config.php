6857c15ceb4d1027ae89b92b086ac422799ee2cf
Mikhail Kormanowsky <mkmkor-kr@yandex.ru>
2018-08-17 16:19:14 +0300

    config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+//Сервер БД MySQL
+define("DB_SERVER","YOUR_SERVER");
+//Имя пользователя 
+define("DB_USER","YOUR_USERNAME");
+//Пароль
+define("DB_PASS","YOUR_PASSWORD");
+//Имя базы
+define("DB_NAME","YOUR_DB_NAME");
+?>
